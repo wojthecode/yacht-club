0cabd7d22c30db1150e1c83403a4cb2fc31dd89f
Wojciech Witkowski <witus85@gmail.com>
2026-02-11 12:10:29 +0100

    Register admin,  update gitignore

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,92 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from club.models import Boat, Member, Event, Role, SilingPermission, WorkTask
+
+
+@admin.register(Member)
+class MemberAdmin(UserAdmin):
+    list_display = (
+        "username", "first_name", "last_name", "role", "siling_permission"
+    )
+    list_filter = ["role", ]
+    add_fieldsets = (
+        (
+            (
+                "User info",
+                {
+                    "fields": (
+                        "username",
+                        "email",
+                        "password1",
+                        "password2",
+                    )
+                }
+            ),
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "role",
+                        "siling_permission",
+                    )
+                },
+            ),
+        )
+    )
+    fieldsets = (
+        (
+            (
+                "User info",
+                {
+                    "fields": (
+                        "username",
+                        "email",
+                        "password",
+                    )
+                }
+            ),
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "role",
+                        "siling_permission",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Role)
+class AdminRole(admin.ModelAdmin):
+    list_display = ("name", "management_rights")
+
+
+@admin.register(SilingPermission)
+class AdminSilingPermission(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Boat)
+class AdminBoat(admin.ModelAdmin):
+    list_display = ("name", "lenght", "club_owner")
+
+
+@admin.register(Event)
+class AdminEvent(admin.ModelAdmin):
+    list_display  = ("name", "date", "created_by")
+
+
+@admin.register(WorkTask)
+class AdminWorkTask(admin.ModelAdmin):
+    list_display  = ("name", "date", "created_by")
+
+
+admin.site.unregister(Group)
