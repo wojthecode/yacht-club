04a7014840a5c513b1c68e3801045003f16ecd6a
Wojciech Witkowski <witus85@gmail.com>
2026-02-11 15:46:03 +0100

    Add events list page

--- club/urls.py
+++ club/urls.py
@@ -1,27 +1,14 @@
-"""
-URL configuration for yacht_club project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
 from django.urls import path
 
-from club.views import index
+from club.views import (
+    index,
+    EventListView,
+)
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path("events/", EventListView.as_view(), name="event-list"),
 ]
 
 app_name = "club"
--- club/views.py
+++ club/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from club.models import Event
 
 
 def index(request:HttpRequest) -> HttpResponse:
     return render(request, "club/index.html")
+
+
+class EventListView(generic.ListView):
+    model = Event
